6e3e28133fa0126e735d0b96b668528f6862adfb
Robert Doughty <Robert_Doughty@Roberts-MacBook-Pro-2.local>
2016-04-07 23:12:37 -0400

    Added some minor stuff to main.php

--- index.php
+++ index.php
@@ -59,7 +59,6 @@ if(isset($_GET['createAccount'])) {
     	<title>Welcome Page</title>
     	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     </head>
-    </head>
 	<body>
 
 		<div class="container">
--- main.php
+++ main.php
@@ -1,11 +1,62 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: AdamBenjamin
- * Date: 4/6/2016
- * Time: 7:11 PM
- */
 
-echo "Welcome to the main page!";
+session_start();
 
+$content ="";
+
+$servername = "localhost";
+$username = "cse385";
+$password = "bizfik19"; 
+$dbname = "cse385";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_GET['query'])) {
+
+
+} else if (isset($_GET['insert'])) {
+
+
+} else if (isset($_GET['delete'])) {
+
+
+} else {
+
+$content .= '<div class="container">';
+
+$content .= '<h1>What would you like to do?</h1>';
+
+$content .= '<div><a href="?query" class="btn btn-primary">Search Menus</a></div>';
+
+$content .= '<div><a href="?insert" class="btn btn-primary">Create New Menu</a></div>';
+
+$content .= '<div><a href="?delete" class="btn btn-primary">Delete Menu</a></div>';
+
+$content .= '</div>';
+
+}
+
+$conn->close();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+    	<title>Main</title>
+    	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    </head>
+	<body>
+
+		<div class="container">
+			<?=$content?>
+		</div>
+		
+	</body>
+</html>
